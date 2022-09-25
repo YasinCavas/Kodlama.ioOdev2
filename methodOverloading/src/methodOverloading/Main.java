434411a0b259971ea8efaa75b9c7ce10a30296d0
YasinCavas <yasincavas16@gmail.com>
2022-09-26 01:57:04 +0300

    methodOverloading

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		dortIslem dortIslem=new dortIslem();
+		System.out.println(dortIslem.topla(5, 7));
+        System.out.println(dortIslem.topla(5, 7, 3));
+		
+
+	}
+
+}
